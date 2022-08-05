a2246556ead934eddb226a35cbae50f2739f6b04
serhiichaiuk <chayck7@gmail.com>
2022-08-05 17:26:09 +0300

    Add dump binary path

--- config/database.php
+++ config/database.php
@@ -58,6 +58,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'dump' => [
+                'dump_binary_path' => 'D:\OpenServer\modules\database\MySQL-5.5\bin',
+            ],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
